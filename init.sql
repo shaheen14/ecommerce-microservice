607837572acdf5a8c161ccc145ca3e951c150ee7
shaheen <shaheenparveen1407@gmail.com>
2025-12-14 23:22:18 +0530

    Day 3: Added created_at column and additional seed data

--- init.sql
+++ init.sql
@@ -2,11 +2,15 @@ CREATE TABLE sales (
     id SERIAL PRIMARY KEY,
     product_name VARCHAR(100),
     amount INTEGER,
-    currency VARCHAR(10)
+    currency VARCHAR(10),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO sales (product_name, amount, currency) VALUES
 ('Laptop', 50000, 'INR'),
 ('Headphones', 3000, 'INR'),
 ('Keyboard', 1500, 'INR');
+INSERT INTO sales (product_name, amount, currency) VALUES ('Mouse', 700, 'INR');
+INSERT INTO sales (product_name, amount, currency) VALUES ('Monitor', 12000, 'INR');
+
 
